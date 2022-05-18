92a0ec9fa51cc1592eaa9021eafa660c8ac5b51c
Maya Ha <mayaha@ucsb.edu>
2022-05-18 12:44:00 -0700

    Init

--- frontend/src/main/components/UCSBDiningCommonsMenu/UCSBDiningCommonsMenuTable.js
+++ frontend/src/main/components/UCSBDiningCommonsMenu/UCSBDiningCommonsMenuTable.js
@@ -1,27 +1,38 @@
-import OurTable/*,{ ButtonColumn }*/from "main/components/OurTable";
-//import { useBackendMutation } from "main/utils/useBackend";
-//import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/utils/UCSBDiningCommonsMenuUtils"
+import OurTable,{ ButtonColumn }from "main/components/OurTable";
+import { useBackendMutation } from "main/utils/useBackend";
+import {onDeleteSuccess } from "main/utils/UCSBDateUtils"
 //import { useNavigate } from "react-router-dom";
-//import { hasRole } from "main/utils/currentUser";
+import { hasRole } from "main/utils/currentUser";
 
-export default function UCSBDiningCommonsMenuTable({ diningCommonsMenu, _currentUser }) {
-
-    /* const navigate = useNavigate();
-
-    const editCallback = (cell) => {
-        navigate(`/ucsbdiningcommonsmenu/edit/${cell.row.values.id}`)
+export function cellToAxiosParamsDelete(cell) {
+    return {
+        url: "/api/ucsbdiningcommonsmenu",
+        method: "DELETE",
+        params: {
+            id: cell.row.values.id
+        }
     }
+}
+
+
+export default function UCSBDiningCommonsMenuTable({ diningCommonsMenu, currentUser }) {
+
+    //const navigate = useNavigate();
+
+    //const editCallback = (cell) => {
+    //    navigate(`/ucsbdiningcommonsmenu/edit/${cell.row.values.id}`)
+    //}
 
     // Stryker disable all : hard to test for query caching
     const deleteMutation = useBackendMutation(
         cellToAxiosParamsDelete,
         { onSuccess: onDeleteSuccess },
-        ["/api/ucsbdates/all"]
+        ["/api/ucsbdiningcommonsmenu/all"]
     );
     // Stryker enable all 
 
     // Stryker disable next-line all : TODO try to make a good test for this
-    const deleteCallback = async (cell) => { deleteMutation.mutate(cell); } */
+    const deleteCallback = async (cell) => { deleteMutation.mutate(cell); } 
 
     const columns = [
         {
@@ -42,14 +53,14 @@ export default function UCSBDiningCommonsMenuTable({ diningCommonsMenu, _current
         }
     ];
 
-/*     const columnsIfAdmin = [
+    const columnsIfAdmin = [
         ...columns,
-        ButtonColumn("Edit", "primary", editCallback, "UCSBDatesTable"),
-        ButtonColumn("Delete", "danger", deleteCallback, "UCSBDatesTable")
-    ]; */
+        //ButtonColumn("Edit", "primary", editCallback, "UCSBDatesTable"),
+        ButtonColumn("Delete", "danger", deleteCallback, "UCSBDiningCommonsMenuTable")
+    ];
 
-    //const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
-    const columnsToDisplay = columns;
+    const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
+    //const columnsToDisplay = columns;
 
     return <OurTable
         data={diningCommonsMenu}
