358cab3407dd11dc2b0f5bc3c9180902522be9a1
Archana Neupane <archananeupane5@gmail.com>
2022-05-18 16:28:40 -0700

    an - replaced placeholder with table

---
+++ frontend/src/stories/components/Organization/OrganizationTableStories.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import UCSBOrganizationTable from "main/components/UCSBOrganization/UCSBOrganizationTable";
+import { ucsbOrganizationFixtures } from 'fixtures/ucsbOrganizationFixtures';
+
+export default {
+    title: 'components/Organization/UCSBOganizationTable',
+    component: UCSBOrganizationTable
+};
+
+const Template = (args) => {
+    return (
+        <UCSBOrganizationTable {...args} />
+    )
+};
+
+export const Empty = Template.bind({});
+
+Empty.args = {
+    organization: []
+};
+
+export const ThreeOrganization = Template.bind({});
+
+ThreeOrganization.args = {
+    organzation: ucsbOrganizationFixtures.ThreeOrganization
+};
+
+
