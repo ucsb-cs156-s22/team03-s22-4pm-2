09acb98832f919b2714a99d508ec795eb4bb563f
Sachen Sampath <56048555+scsampath@users.noreply.github.com>
2022-05-18 23:44:44 -0700

    Create HelpRequestTable.stories.js

---
+++ frontend/src/stories/components/HelpRequest/HelpRequestTable.stories.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import HelpRequestTable from "main/components/HelpRequest/HelpRequestTable";
+import { helpRequestFixtures } from 'fixtures/helpRequestFixtures';
+
+export default {
+    title: 'components/HelpRequest/HelpRequestTable',
+    component: HelpRequestTable
+};
+
+const Template = (args) => {
+    return (
+        <HelpRequestTable {...args} />
+    )
+};
+
+export const Empty = Template.bind({});
+
+Empty.args = {
+    helpRequests: []
+};
+
+export const ThreeHelpRequests = Template.bind({});
+
+ThreeHelpRequests.args = {
+    helpRequests: helpRequestFixtures.threeHelpRequests
+};
+
+
