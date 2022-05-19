6c9a5ca32e4b7c0048480929f4746356bf99374f
E-R-E-N <38056364+E-R-E-N@users.noreply.github.com>
2022-05-19 14:46:10 -0700

    Update MenuItemReviewsIndexPage.js

--- frontend/src/main/pages/MenuItemReviews/MenuItemReviewsIndexPage.js
+++ frontend/src/main/pages/MenuItemReviews/MenuItemReviewsIndexPage.js
@@ -10,7 +10,9 @@ export default function MenuItemReviewIndexPage() {
 
   const { data: menuItemReviews, error: _error, status: _status } =
     useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
       ["/api/MenuItemReview/all"],
+            // Stryker disable next-line StringLiteral,ObjectLiteral : since "GET" is default, "" is an equivalent mutation
             { method: "GET", url: "/api/MenuItemReview/all" },
       []
     );
